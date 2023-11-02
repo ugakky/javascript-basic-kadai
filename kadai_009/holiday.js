a987743b987fc6385bd745159f6e8715d2107dcd
ugakky <144716479+ugakky@users.noreply.github.com>
2023-11-02 18:43:14 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,12 @@
+const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
+//let  Lastarray = holidays.length;
+//console.log(Lastarray);
+let tmp=holidays[0];
+//console.log(Lastarray);
+for(let i=0; i<holidays.length ;i++)
+{
+  tmp=holidays[i];
+  console.log(tmp);
+}
+
+console.log(holidays);
